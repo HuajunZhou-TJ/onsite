0d5856806d04feecb5f2f1412aac27b1c4cb6dfa
Zhou Huajun <1931314@tongji.edu.cn>
2021-11-25 15:10:26 +0800

    去掉了奇怪的注释

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-import setuptools #导入setuptools打包工具
+import setuptools 
  
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
  
 setuptools.setup(
-    name="onsite", # 用自己的名替换其中的YOUR_USERNAME_
-    version="0.0.1",    #包版本号，便于维护版本
-    author="Zhou Huajun",    #作者，可以写自己的姓名
-    author_email="1931314@tongji.edu.cn",    #作者联系方式，可写自己的邮箱地址
-    description="A small example package",#包的简述
-    long_description=long_description,    #包的详细介绍，一般在README.md文件内
+    name="onsite", 
+    version="0.0.1",   
+    author="Zhou Huajun",   
+    author_email="1931314@tongji.edu.cn",   
+    description="A tool packege for the competition of Onsite",
+    long_description=long_description,    
     long_description_content_type="text/markdown",
-    # url="https://github.com/pypa/sampleproject",    #自己项目地址，比如github的项目地址
+    url="https://github.com/HuajunZhou-TJ/onsite", 
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
